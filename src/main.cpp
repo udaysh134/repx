790b1c7cc99ac07d8b931941f78ac8f5f1bbd6f2
Uday Shukla <udaysh8351@gmail.com>
2026-02-04 01:39:26 +0530

    feat: update main entry point with module wiring

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 using namespace std;
 
+#include <new.hpp>
+#include <open.hpp>
+#include <exit.hpp>
+
+
 int main() {
+    int input;
+
     cout <<
-    "Create New" << endl << 
-    "Open Existing" << endl <<
-    "Exit";
+    "1. Create New" << endl << 
+    "2. Open Existing" << endl <<
+    "3. Exit";
+
+    cin >> input;
+
+    switch (input) {
+        case 1:
+
+            break;
+        case 2:
+        
+            break;
+        case 3:
+        
+            break;
+        default:
+            break;
+    }
 }
