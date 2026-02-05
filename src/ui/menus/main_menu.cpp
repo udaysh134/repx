71cf4f608046a6cc9acf93a921e10a6822cebf4b
Uday Shukla <udaysh8351@gmail.com>
2026-02-06 01:54:05 +0530

    feat: render interactive main menu using pdcurses

--- src/ui/menus/main_menu.cpp
+++ src/ui/menus/main_menu.cpp
@@ -1,9 +1,57 @@
+// Headers
 #include <curses.h>
 
+#include "../screen.hpp"
 #include "main_menu.hpp"
-#include "../app_state.hpp"
 
-AppState runMainMenu() {
-    getch();
-    return AppState::MAIN_MENU;
+// Constants
+static const char* ITEMS[] = { "New", "Open", "Exit" };
+static const int ITEM_COUNT = 3;
+
+
+/*
+----------------------------------------------------------------------------------------------------
+MAIN MENU
+----------------------------------------------------------------------------------------------------
+*/
+Screen runMainMenu() {
+    int selected = 0;
+
+    while (true) {
+        clear();
+
+        mvprintw(2, 4, "RepX");
+
+        for (int i = 0; i < ITEM_COUNT; i++) {
+            if (i == selected) {
+                attron(A_REVERSE);
+            }
+
+            mvprintw(5 + i, 6, ITEMS[i]);
+
+            if (i == selected) {
+                attroff(A_REVERSE);
+            }
+        }
+
+        refresh();
+
+        int ch = getch();
+
+        switch (ch) {
+            case KEY_UP:
+                selected = (selected - 1 + ITEM_COUNT) % ITEM_COUNT;
+                break;
+
+            case KEY_DOWN:
+                selected = (selected + 1) % ITEM_COUNT;
+                break;
+
+            case '\n':
+                if (selected == 0) return Screen::NEW_MENU;
+                if (selected == 1) return Screen::OPEN_MENU;
+                if (selected == 2) return Screen::EXIT_MENU;
+                break;
+        }
+    }
 }
