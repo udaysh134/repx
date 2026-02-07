6b220fc40afb379d9d18ef850c4a9281372d7061
Uday Shukla <udaysh8351@gmail.com>
2026-02-08 01:58:00 +0530

    chore: add centred title and improve running control mechanism

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -12,9 +12,31 @@ void start() {
     int width, height;
 
     console::getTerminalSize(width, height);
-    console::drawBox(0, 0, width, height, '-', '|', '+');
 
-    while (true) {
+    int headerH = 5;
+    int footerH = 5;
+    int bodyH = height - headerH - footerH;
+
+    char h_char = '-';
+    char v_char = '|';
+    char c_char = '+';
+
+    // Header Box
+    console::drawBox(0, 0, width, headerH, h_char, v_char,c_char);
+    // Body Box
+    console::drawBox(0, headerH, width, bodyH, h_char, v_char,c_char);
+    // Footer Box
+    console::drawBox(0, headerH + bodyH, width, footerH, h_char, v_char,c_char);
+
+    std::string title = "RepX";
+    int titleX = (width - title.length()) / 2;
+    int titleY = 2;
+
+    console::moveCursor(titleX, titleY);
+    std::cout << title;
+
+    bool  running = true;
+    while (running) {
         
     }
 }
