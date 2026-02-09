b5785679d6c962208f10d8c0bdc9318cff229420
Uday Shukla <udaysh8351@gmail.com>
2026-02-09 12:11:08 +0530

    feat: add minimum screen size handling with centered error UI
    Implemented minimum terminal size checks and a centered error display before rendering the main layout.

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -1,8 +1,14 @@
 // Headers
 #include <iostream>
+#include <string>
+#include <conio.h>
 
 #include "utils.hpp"
 #include "layout.hpp"
+
+// Constants
+constexpr int MIN_SCR_WIDTH  = 60;
+constexpr int MIN_SCR_HEIGHT = 20;
 
 
 /*
@@ -12,8 +18,24 @@ MAIN LOOP
 */
 void start() {
     int width, height;
-
     console::getTerminalSize(width, height);
+
+    if (width < MIN_SCR_WIDTH || height < MIN_SCR_HEIGHT) {
+        std::string errLine_1 = "Error : Terminal too small";
+        std::string errLine_2 = "Minimum required screen resolution : " + std::to_string(MIN_SCR_WIDTH) + " x " + std::to_string(MIN_SCR_HEIGHT);
+
+        console::clearScreen();
+
+        int centerY = height / 2;
+        int err1X = (width - errLine_1.length()) / 2;
+        int err2X = (width - errLine_2.length()) / 2;
+
+        console::moveCursor(err1X, centerY - 1);
+        std::cout << color::RED << errLine_1 << color::RESET;
+
+        console::moveCursor(err2X, centerY);
+        std::cout << color::BLUE << errLine_2 << color::RESET;
+    }
 
     int headerH = 5;
     int footerH = 5;
@@ -36,6 +58,8 @@ void start() {
 
     console::moveCursor(titleX, titleY);
     std::cout << title;
+
+    getch();
 
     bool  running = true;
     while (running) {
