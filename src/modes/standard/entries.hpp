4a677115c6b760591c382eabb9e0744e3ee22f9f
Uday Shukla <udaysh8351@gmail.com>
2026-06-28 05:48:35 +0530

    refactor: modify Ledger Runtime and Entry schemas and implement them in Standard Mode
    Added a fully implemented schema for runtime type and entry type for standard mode, modifying from the originally built Ledger Mode's runtime and entry files. Also updated session.hpp according to the new changes.

---
+++ src/modes/standard/entries.hpp
@@ -0,0 +1,174 @@
+#pragma once
+
+#include <string>
+
+#include "typealias.hpp"
+
+
+struct Genesis {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    schema_t schema;
+    std::string engine;
+    std::string mode;
+    struct Project {
+        std::string name;
+        std::string description;
+    } project;
+    struct Owner {
+        userID_t uid;
+        std::string name;
+        struct Auth {
+            std::string salt;
+            std::string hash;
+        } auth;
+    } owner;
+};
+
+
+struct SubjectCreate {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    userID_t uid;
+    std::string name;
+    std::string reason;
+    userID_t author;
+};
+
+
+struct SubjectDeactivate {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    userID_t target;
+    std::string reason;
+    userID_t author;
+};
+
+
+struct SubjectReactivate {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    userID_t target;
+    std::string reason;
+    userID_t author;
+};
+
+
+struct Rep {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    userID_t target;
+    delta_t delta;
+    std::string reason;
+    userID_t author;
+};
+
+
+struct AdminAssign {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    struct Admin {
+        userID_t uid;
+        std::string name;
+        struct Auth {
+            std::string salt;
+            std::string hash;
+        } auth;
+    } admin;
+    std::string reason;
+    userID_t author;
+};
+
+
+struct AdminRevoke {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    userID_t target;
+    std::string reason;
+    userID_t author;
+};
+
+
+struct AdminRestore {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    userID_t target;
+    std::string reason;
+    userID_t author;
+};
+
+
+struct UserRename {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    userID_t target;
+    std::string name;
+    std::string reason;
+    userID_t author;
+};
+
+
+struct ChangePass {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    userID_t target;
+    struct Auth {
+        std::string salt;
+        std::string hash;
+    } auth;
+    std::string reason;
+    userID_t author;
+};
+
+
+struct ProjectUpdate {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    struct Project {
+        std::string name;
+        std::string description;
+    } project;
+    std::string reason;
+    userID_t author;
+};
+
+
+struct ConfigUpdate {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    struct Config {
+        // Empty intentionally, data fields unknown
+    } config;
+    std::string reason;
+    userID_t author;
+};
+
+
+struct Archive {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    std::string reason;
+    userID_t author;
+};
+
+
+struct Unarchive {
+    entryID_t id;
+    timeMS_t timestamp;
+    std::string type;
+    std::string reason;
+    userID_t author;
+};
